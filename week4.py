89e653ac0c6881b9e09a3cf9b7f0e093473d42a2
Ryan Frank <rfrank@my365.bellevue.edu>
2025-10-05 21:13:06 -0400

    Add files via upload

---
+++ week4.py
@@ -0,0 +1,28 @@
+
+from pyspark.sql import SparkSession
+import random
+
+# Start Spark
+spark = SparkSession.builder.appName("SentenceGenerator").getOrCreate()
+sc = spark.sparkContext
+
+# Word list
+words = ["apple", "banana", "cherry", "date", "elderberry", "fig", "grape", "honeydew"]
+
+# Generate sentences on the driver
+num_sentences = 1000
+sentences = [
+    " ".join(random.sample(words, random.randint(1, 6))) + "."
+    for _ in range(num_sentences)
+]
+
+# Parallelize into RDD
+sentences_rdd = sc.parallelize(sentences)
+
+# Transformation (capitalize first letter of sentence)
+transformed = sentences_rdd.map(lambda s: s.capitalize())
+
+# Save to HDFS (change the path to something you have write access to)
+output_path = "hdfs:///tmp/week4_output"
+transformed.saveAsTextFile(output_path)
+spark.stop()
