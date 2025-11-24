30df89ef12f52fa9135f165562eced7496ccde69
Ryan Frank <rfrank@my365.bellevue.edu>
2025-11-23 21:19:12 -0500

    Update finalProject.py

--- finalProject.py
+++ finalProject.py
@@ -12,4 +12,4 @@ salaryDF = spark.sql("SELECT workYear, experienceLevel, employmentType, jobTitle
                      "salaryInUSD, employeeResidence, remoteRatio, companyLocation, companySize "
                      "FROM dsSalaries")
 
-salaryDF.head()
+println(salaryDF.head())
